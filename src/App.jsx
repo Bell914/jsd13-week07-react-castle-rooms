870506561b2ef2352310f4698996ad670503b65f
Bell914 <ittikorn.tipson@gmail.com>
2026-08-13 15:47:02 +0700

    add 01_Castle,02_Tower

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Castle from "./components/01_Castle";
+
+export default function App() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white pb-80 py-10 gap-y-4">
+      <Castle />
+    </div>
+  );
+}
